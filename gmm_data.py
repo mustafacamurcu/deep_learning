3fab8b2aebf5d293e44e310954e97154a20d3c18
kocabey <kocabey@mit.edu>
2016-05-13 00:27:37 -0400

    minor

--- gmm_data.py
+++ gmm_data.py
@@ -44,7 +44,9 @@ def prepare_bird_representation(all_data):
                     if j != k and j != l and k != l:
                         a = [data[j][0] - data[k][0], data[j][1] - data[k][1]]
                         b = math.sqrt( (data[k][0] - data[l][0]) ** 2 + (data[k][1] - data[l][1]) ** 2 )
-                        representation[i][ctr] = a/b
+
+                        representation[i][ctr][0] = a[0]/b[0]
+                        representation[i][ctr][1] = a[1]/b[1]
 
                         if data[j][2] == 0 or data[k][2] == 0 or data[l][2] == 0:
                             representation[i][ctr] = (0,0)
