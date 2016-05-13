ac23c13849edf0317dcdc3b3bf13d112eb0be25f
kocabey <kocabey@mit.edu>
2016-05-13 00:37:52 -0400

    minor

--- bird_evaluator.py
+++ bird_evaluator.py
@@ -117,7 +117,7 @@ def model_accuracy(sess, part_id):
         image_id = int("".join(jpg[i].split(".jpg")).split("Test/")[1]) + 1
         batch_x = np.zeros((1,224,224,3))
         batch_x[0,:,:,:,] = VGG_utils.image_preprocess(jpg[i])
-        mx,my,f_c = sess.run([mean_x,mean_y,guess], feed_dict = {x: batch_x})
+        mx,my,f_c = sess.run([mean_x,mean_y,fc], feed_dict = {x: batch_x})
 
 
         visibility = ground_truth[image_id][part_id][2]
