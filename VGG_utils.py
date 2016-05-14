03bba194741a5814488cd3f57b75aa7beece9874
Mustafa Camurcu <camurcu.m@husky.neu.edu>
2016-05-14 16:47:28 -0400

    lfpw train

--- VGG_utils.py
+++ VGG_utils.py
@@ -103,8 +103,8 @@ def get_next_trn_batch_face_lfpw(all_data):
     for i in range(BATCH_SIZE):
         (aa,bb,cc) = imread( all_data[trn_indices[i]][0] ).shape
         for j in range(68):
-            batch_point_x[i][j] = all_data[trn_indices[i]][1][0][j][0] * 10 / float(aa)
-            batch_point_y[i][j] = all_data[trn_indices[i]][1][0][j][1] * 10 / float(aa)
+            batch_point_x[i][j] = all_data[trn_indices[i]][1][j][0] * 10 / float(aa)
+            batch_point_y[i][j] = all_data[trn_indices[i]][1][j][1] * 10 / float(aa)
 
     return (batch_x,batch_point_x,batch_point_y)
 
@@ -142,8 +142,8 @@ def get_next_trn_caltech_batch_face(all_data):
     for i in range(BATCH_SIZE):
         (aa,bb,cc) = imread( all_data[trn_indices[i]][0] ).shape
         for j in range(4):
-            batch_point_x[i][j] = all_data[trn_indices[i]][1][ 2*j ] * 10 / float(aa)
-            batch_point_y[i][j] = all_data[trn_indices[i]][1][2*j+1] * 10 / float(aa)
+            batch_point_x[i][j] = all_data[trn_indices[i]][1][0][ 2*j ] * 10 / float(aa)
+            batch_point_y[i][j] = all_data[trn_indices[i]][1][0][2*j+1] * 10 / float(aa)
 
     return (batch_x,batch_point_x,batch_point_y)
 
